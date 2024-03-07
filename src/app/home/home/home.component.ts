57cf0d28f283b401bb54528d25263e8e32dc2bf9
karimul <7mdkarimul@gmail.com>
2024-03-07 07:19:38 +0530

    commit after home page designed

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -6,13 +6,15 @@ import { BenefitComponent } from "../benefit/benefit.component";
 import { FeaturesComponent } from "../features/features.component";
 import { TestimonialComponent } from "../testimonial/testimonial.component";
 import { FooterComponent } from "../footer/footer.component";
+import { PricingComponent } from "../pricing/pricing.component";
+import { TeamComponent } from "../team/team.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [NavbarComponent, HeroComponent, BrandComponent, BenefitComponent, FeaturesComponent, TestimonialComponent, FooterComponent]
+    imports: [NavbarComponent, HeroComponent, BrandComponent, BenefitComponent, FeaturesComponent, TestimonialComponent, FooterComponent, PricingComponent, TeamComponent]
 })
 export class HomeComponent {
 
